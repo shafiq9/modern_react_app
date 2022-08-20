3bebd69ff7a897e1aac3e2a72ca0ae025ad6db84
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2022-08-20 19:04:37 +0500

    error fix primary in app js

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,13 @@ const App = () => (
             </div>
         </div>
 
-        <div className={`bg-parimary ${styles.flexStart}`}>
+        <div className={`bg-primary ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
                 <Hero />
             </div>
         </div>
 
-        <div className={`bg-parimary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
             <div className={`${styles.boxWidth}`}>
                 <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />
                 <Clients /> <CTA /> <Footer />
