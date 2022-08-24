a60ff3ddc415b4ac2dace3c5340b00670480dd9f
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2022-08-24 21:39:26 +0500

    client, CTA, FeedbackCard, Testimonials and Footer section done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const Clients = () => {
-    return <div>Clients</div>;
-};
+import { clients } from "../constants";
+import styles from "../style";
+
+const Clients = () => (
+    <section className={`${styles.flexCenter} m-4 `}>
+        <di className={`${styles.flexCenter} flex-wrap w-full `}>
+            {clients.map((clients)=>(
+                <div key={clients.id} className={`flex-1 
+                    ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+                    <img src={clients.logo} alt="client"
+                    className="sm:w-[192px] w-[100px]
+                    object.contain" />
+                </div>
+            ))}
+        </di>
+    </section>
+)
 
 export default Clients;
